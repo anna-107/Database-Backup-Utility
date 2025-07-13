35554049f81f3ce67f81d4f2c8f0c655a7e9e549
anna-107 <anonymousisgod61@gmail.com>
2025-07-14 01:15:28 +0530

    Backup and Restore done

--- src/main/java/com/backup/Main.java
+++ src/main/java/com/backup/Main.java
@@ -27,6 +27,7 @@ public class Main {
         System.out.print("\nEnter Database Type (mysql/mongodb): ");
         String dbType = scanner.nextLine().trim().toLowerCase();
 
+
         DatabaseService service = switch (dbType) {
             case "mysql"   -> new MySQLService();
             case "mongodb" -> new MongoDBService();
@@ -41,17 +42,27 @@ public class Main {
 
         boolean ok = service.testConnection();
         if (ok) {
-            System.out.println("\n Connection test succeeded.");
-            service.fullBackup();
+            System.out.println("\nConnection test succeeded.");
+            System.out.println("Do you want to backup or restore?\n" +
+                    "Press 1 for backup, press 0 for restore!");
+            int choice2= Integer.parseInt(scanner.nextLine().trim());
+            if(choice2==1){
+            service.fullBackup();}
+            else{
+                System.out.println("Enter the path of your backup file: ");
+                String path=scanner.nextLine().trim();
+                service.fullRestore(path);
+            }
+
         } else {
-            System.out.println("\n Connection test failed. Fix credentials/network and retry.");
+            System.out.println("\nConnection test failed. Fix credentials/network and retry.");
         }
     }
 
     private static void showHelp() {
-        System.out.println("\n=== HELP MENU ===");
+        System.out.println("\n================= HELP MENU =================");
         System.out.println("1  → start interactive setup");
         System.out.println("0  → show this menu / exit");
-        System.out.println("The tool currently checks connectivity to MySQL or MongoDB.");
+        System.out.println("The tool currently checks connectivity to MySQL or MongoDB. It can create a backup of your database!");
     }
 }
